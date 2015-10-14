454839ee77069a43cb6fcdcedccb92ec3eb8d9ad
Steven La <mrstevenla@gmail.com>
2015-10-13 21:33:56 -0700

    Add null check to handleClickOutside

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ module.exports = function enhanceWithClickOutside(WrappedComponent) {
     },
 
     handleClickOutside(e) {
-      if (!React.findDOMNode(this).contains(e.target)) {
+      var domNode = React.findDOMNode(this);
+      if (!domNode || !domNode.contains(e.target)) {
         this.refs.wrappedComponent.handleClickOutside(e);
       }
     },
