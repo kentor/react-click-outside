0cb99e2a555b0a9074cc3868fd0139cc7175ecd8
Christopher Pappas <damassi.pappas@gmail.com>
2015-10-22 22:42:57 -0700

    Upgrade to React 0.14

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 module.exports = function enhanceWithClickOutside(WrappedComponent) {
   if (!WrappedComponent.prototype.handleClickOutside) {
@@ -22,7 +23,7 @@ module.exports = function enhanceWithClickOutside(WrappedComponent) {
     },
 
     handleClickOutside: function handleClickOutside(e) {
-      var domNode = React.findDOMNode(this);
+      var domNode = ReactDOM.findDOMNode(this);
       if (!domNode || !domNode.contains(e.target)) {
         this.refs.wrappedComponent.handleClickOutside(e);
       }
