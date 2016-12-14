5ec4695585dbfaaa911066a0a9bcb2cccc3e9100
Kenneth Chung <ken70r@gmail.com>
2016-12-13 21:51:16 -0800

    Add demo for testing purposes
    This is useful when testing on a mobile device

---
+++ demo/app.js
@@ -0,0 +1,36 @@
+const enhanceWithClickOutside = require('../index');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const style = {
+  backgroundColor: '#fff',
+  border: '1px solid #000',
+  height: 100,
+  width: 100,
+};
+
+const Target = enhanceWithClickOutside(React.createClass({
+  handleClickOutside() {
+    const hue = Math.floor(Math.random() * 360);
+    document.body.style.backgroundColor = `hsl(${hue}, 100%, 87.5%)`;
+  },
+
+  render() {
+    const isMobile = 'ontouchstart' in document.body;
+    return <div style={style}>{`mobile: ${isMobile}`}</div>;
+  },
+}));
+
+const Root = () => (
+  <div>
+    <Target />
+    <button style={style}>Button Element</button>
+  </div>
+);
+
+if ('ontouchstart' in document.documentElement) {
+  document.body.style.cursor = 'pointer';
+  document.documentElement.style.touchAction = 'manipulation';
+}
+
+ReactDOM.render(<Root />, document.body);
